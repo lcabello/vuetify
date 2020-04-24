7ddc442c84469015bb935ddc9aff07e60ddfe10e
LCabello <luis.cab.fer@gmail.com>
2020-04-24 17:50:11 +0200

    New view to explore veutify grid

---
+++ src/views/Grid.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12" sm="6">
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus consectetur delectus aperiam in maiores quod officiis nam at unde fuga tenetur ad reprehenderit nostrum tempore voluptas culpa nisi, quae a?</p>
+      </v-col>
+      <v-col cols="12" sm="6">
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus consectetur delectus aperiam in maiores quod officiis nam at unde fuga tenetur ad reprehenderit nostrum tempore voluptas culpa nisi, quae a?</p>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
