ff60bfe8d1479775dfa1cd1ff012f9a3c3310bbd
LCabello <luis.cab.fer@gmail.com>
2020-04-24 18:12:30 +0200

    Testing grids properties

--- src/views/Grid.vue
+++ src/views/Grid.vue
@@ -8,5 +8,8 @@
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus consectetur delectus aperiam in maiores quod officiis nam at unde fuga tenetur ad reprehenderit nostrum tempore voluptas culpa nisi, quae a?</p>
       </v-col>
     </v-row>
+    <v-row class="mt-5" justify="center">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam consequuntur pariatur omnis, accusamus, necessitatibus assumenda, odio non vel saepe iste illum eius provident porro qui minima et labore harum doloremque.</p>
+    </v-row>
   </v-container>
 </template>
